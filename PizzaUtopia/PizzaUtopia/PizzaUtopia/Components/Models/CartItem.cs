d69cc2c5662db341736849f8f2eea231c8a27f05
Kacper Kosal <kacper.kosal@op.pl>
2024-11-21 14:42:03 +0100

    Dodanie koszyka

---
+++ PizzaUtopia/PizzaUtopia/PizzaUtopia/Components/Models/CartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaUtopia.Components.Models
+{
+    public class CartItem
+    {
+        public string Name { get; set; } = string.Empty;
+        public string ImageUrl => $"{Name.ToLower().Replace(" ", "-")}-pizza.png";
+        public int Quantity { get; set; }
+    }
+}
