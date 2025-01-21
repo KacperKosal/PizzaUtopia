8aec79d4d3d38c4dbeaa27c7736984fe1a16a6eb
KacperKosal <kacper.kosal@op.pl>
2025-01-21 10:28:57 +0100

    bazadanychlogowanie

---
+++ PizzaUtopia/PizzaUtopia/PizzaUtopia.Backend/Endpoints/LoginEndPoint.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaUtopia.Backend.Context;
+using PizzaUtopia.Backend.Dto;
+
+namespace PizzaUtopia.Backend.Endpoints
+
+{
+    public class LoginEndPoint(PizzaUtopiaDbContext dbContext)
+    {
+        public async Task<string> Login(LoginDataDto loginData)
+        {
+            var user = await dbContext.Users.FirstOrDefaultAsync(u => u.Email == loginData.Email && u.PasswordHash == loginData.Password);
+
+            if (user == null)
+            {
+                return "Niepoprawne dane logowania";
+            }
+            else
+            {
+                return "Zalogowano";
+            }
+        }
+    }
+}
--- PizzaUtopia/PizzaUtopia/PizzaUtopia.Backend/Program.cs
+++ PizzaUtopia/PizzaUtopia/PizzaUtopia.Backend/Program.cs
@@ -1,23 +1,49 @@
 using PizzaUtopia.Backend.Context;
+using PizzaUtopia.Backend.Dto;
+using PizzaUtopia.Backend.Endpoints;
 using PizzaUtopia.Backend.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Ustawienia URL i portu
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5000); // Nas³uchiwanie na porcie 5000 na wszystkich interfejsach
+    options.ListenAnyIP(5001, listenOptions =>
+    {
+        listenOptions.UseHttps(); // Jeœli chcesz obs³ugiwaæ HTTPS na porcie 5001
+    });
+});
 
+// Dodaj us³ugi do kontenera
+builder.Services.AddOpenApi();
 builder.Services.AddDbContext<PizzaUtopiaDbContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Konfiguracja potoku ¿¹dañ HTTP
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.MapGet("/", () => "Backend serwisu PizzaUtopia");
+
+app.MapPost("/auth/login", async (PizzaUtopiaDbContext dbContext, LoginDataDto loginData) =>
+{
+    var loginEndPoint = new LoginEndPoint(dbContext);
+
+    var result = await loginEndPoint.Login(loginData);
+
+    if (result == "Niepoprawne dane logowania")
+    {
+        return Results.BadRequest(result);
+    }
+    else
+    {
+        return Results.Ok(result);
+    }
+});
 
 app.MapPost("/register", (PizzaUtopiaDbContext dbContext, User user) =>
 {
@@ -27,4 +53,3 @@ app.MapPost("/register", (PizzaUtopiaDbContext dbContext, User user) =>
 });
 
 app.Run();
-
