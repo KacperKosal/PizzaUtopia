8e9760d197dbcace604f3f8f4043f97f060871f7
kadzior <adriankadziolka18@gmail.com>
2025-01-21 12:16:09 +0100

    Poprawa kodu

---
+++ PizzaUtopia/PizzaUtopia.Backend/Context/PizzaUtopiaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using PizzaUtopia.Backend.Models;
+
+namespace PizzaUtopia.Backend.Context;
+
+public class PizzaUtopiaDbContext(IConfiguration configuration) : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql(configuration.GetValue<string>("MySQLConnectionString") ?? throw new ArgumentNullException("MySQLConnectionString", "Connection string nie może być pusty"), ServerVersion.AutoDetect(configuration.GetValue<string>("MySQLConnectionString") ?? throw new ArgumentNullException("MySQLConnectionString")));
+    }
+}
